d51b0de7e62291f9a7266004ee4d4f5705628002
zeraiadhanom <zeraiadhanom@gmail.com>
2022-07-09 18:32:36 -0700

    image updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
-//import CarouselPage from './CarouselPage'
+
 
  
 class Home extends Component {
   render() {
     return (
       <div className="Home">
-      <div class="top-left" style={{position: "absolute", top: "150px", left: "16px", "font-size": "20px", "text-align": "center", "color": "#FFFF00"}}>
+      <div class="top-left" style={{position: "absolute", top: "150px", left: "16px", "font-size": "20px", "text-align": "center", "color": "black"}}>
          <p>እንቛዕ ኣብ ድሕረ-ገጽ ቤተ ክረስትያን ኪዳነ-ምህረት ብሰላም መጻኩም!</p>
-          “Come home with me, and refresh yourself, <br></br>
-            and I will give you a reward.” Kings 13:7   
+          “For where two or three gather in my name, <br></br>
+            there am i with them.” Mathew 18:20   
       </div>
       </div>  
       
@@ -18,4 +18,7 @@ class Home extends Component {
 }
  
 export default Home
+//"For where two or three gather in my name, 
+//   there am i with them." Mathew 18:20
+
 
